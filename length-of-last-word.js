65123c2a5c372531eba50a33739cd8936e9bd410
Akash Bhaskar <i.am.akashbhaskar@gmail.com>
2024-09-19 23:31:16 -0700

    length-of-last-word

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-  const split = s.split("");
+  const trim = s.trim();
+  const split = trim.split("");
   for (let i = split.length - 1; i > 0; i--) {
-    if (split[i] == " ") {
+    if (split[i] == " " && i < split.length - 1) {
       return split.length - 1 - i;
     }
   }
