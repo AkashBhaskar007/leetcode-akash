93b4e202216623cc9dc34d2fde6d7a8fd8c6459d
Akash Bhaskar <i.am.akashbhaskar@gmail.com>
2024-10-04 14:49:14 +0530

    remove duplcate

--- plus-one.js
+++ plus-one.js
@@ -6,8 +6,6 @@ var plusOne = function (digits) {
   console.log(digits);
   const string = digits.toString().replace(/,/g, "");
   const newN = BigInt(string) + BigInt(1);
-  console.log(newN);
-
   const newArray = Array.from(String(newN), Number);
   return newArray;
 };
---
+++ remove_duplicate.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+  let seen = new Set();
+  while (head) {
+    if (!seen.has(head.val)) {
+      seen.add(head.val);
+      current.next = new ListNode(head.val);
+      current = current.next;
+    }
+    head = head.next;
+  }
+
+  return dummy.next;
+};
+let head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3))));
+let result = deleteDuplicates(head);
+const printList = (node) => {
+  let result = [];
+  while (node) {
+    result.push(node.val);
+    node = node.next;
+  }
+  console.log(result);
+};
+
+printList(result);
